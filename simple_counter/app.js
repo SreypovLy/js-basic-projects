eb78c61d9efedbf2243f9c05864582bd901b08ef
lysreypov <ly.sreypov19@kit.edu.kh>
2023-04-03 16:39:34 +0700

    update button clicked logic

--- simple_counter/app.js
+++ simple_counter/app.js
@@ -1,38 +1,27 @@
-const increaseBtn = document.getElementById("increase");
-const resetBtn = document.getElementById("reset");
-const decreaseBtn = document.getElementById("decrease");
 const number = document.querySelector(".number");
+const buttons = document.querySelectorAll(".btn");
 
 let count = 0;
-increaseBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  count += 1;
-  number.textContent = count;
-  if (count > 0) {
-    number.style.color = "#2ed47b";
-  } else if (count === 0) {
-    number.style.color = "#000000";
-  } else {
-    number.style.color = "#ea2d2d";
-  }
-});
 
-resetBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  count = 0;
-  number.textContent = count;
-  number.style.color = "#000000";
-});
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const style = e.currentTarget.classList;
+    if (style.contains("increase")) {
+      count++;
+    } else if (style.contains("decrease")) {
+      count--;
+    } else {
+      count = 0;
+    }
 
-decreaseBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  count -= 1;
-  number.textContent = count;
-  if (count < 0) {
-    number.style.color = "#ea2d2d";
-  } else if (count === 0) {
-    number.style.color = "#000000";
-  } else {
-    number.style.color = "#2ed47b";
-  }
+    if (count > 0) {
+      number.style.color = "#2ed47b";
+    } else if (count < 0) {
+      number.style.color = "#ea2d2d";
+    } else {
+      number.style.color = "#000000";
+    }
+    number.textContent = count;
+  });
 });
